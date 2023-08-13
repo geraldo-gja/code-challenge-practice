5f334d5f3f51b86f7b1ae86c642956e0205a4bb5
Geraldo <geraldo.gja@gmail.com>
2023-08-13 19:58:33 -0300

    Prova 10.07.23

--- src/critical/techworks/teste2/Prova.java
+++ src/critical/techworks/teste2/Prova.java
@@ -12,12 +12,13 @@ import java.util.stream.Collectors;
 public class Prova {
 
 	public static void main(String[] args) {
-		onlyOddNumbers();
-		duplicateNumbers();
-		duplicateDrivers();
-		testeReferencia();
+		//onlyOddNumbers();
+		//duplicateNumbers();
+		groupByName();
+		//testeReferencia();
 	}
 	
+	//Q01
 	public static void onlyOddNumbers(){
 	    
 		final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9); 
@@ -30,40 +31,41 @@ public class Prova {
 	    System.out.println(output);    
 	}
 	
+	//Q02
 	public static void duplicateNumbers(){
 	    final List<Integer> numbers = Arrays.asList(1, 9,2, 3, 4, 6, 5, 6, 7, 8, 9, 1);  
 	    final Set<Integer> output = new HashSet<Integer>(numbers); 
-	    System.out.println(output);    
+	    System.out.println(output);
 	}
 	
-	public static void duplicateDrivers(){
-		List<Driver> drivers = Arrays.asList(
-	            new Driver("João"),
-	            new Driver("João"),
-	            new Driver("Maria"),
-	            new Driver("Maria"),
-	            new Driver("Vanda"),
-	            new Driver("Camilo"),
-	            new Driver("Camilo"),
-	            new Driver("Luis")
+	//Q03
+	public static void groupByName(){
+		List<Q03Driver> drivers = Arrays.asList(
+	            new Q03Driver("João"),
+	            new Q03Driver("João"),
+	            new Q03Driver("Maria"),
+	            new Q03Driver("Maria"),
+	            new Q03Driver("Vanda"),
+	            new Q03Driver("Camilo"),
+	            new Q03Driver("Camilo"),
+	            new Q03Driver("Luis")
 	    ); 		
-	    Map<String, List<Driver>> driverByName = new HashMap<>();
-	    
-	    for (int i = 0; i < drivers.size(); i++) {
-		
-	    	Driver d = drivers.get(i);
+	    Map<String, List<Q03Driver>> driverByName = new HashMap<>(); 
+	    for (Q03Driver d : drivers) {
 	    	if( driverByName.containsKey(d.getName()) ) {
-	    		List<Driver> list = driverByName.get(d.getName());
+	    		List<Q03Driver> list = driverByName.get(d.getName());
 	    		list.add(d);
 	    	}else {
-	    		List<Driver> list = new LinkedList<>();
+	    		List<Q03Driver> list = new LinkedList<>();
 	    		list.add(d);
 	    		driverByName.put( d.getName(), list );    		
 	    	}
 		}
-	    System.out.println(driverByName);    
+	    for (List<Q03Driver> listDrivers : driverByName.values())
+			System.out.println(listDrivers); 
 	}
 	
+	//Q04
 	public static void testeReferencia() {
 		String a1 = "Java";
 		String a2 = new String("Java");
@@ -71,10 +73,10 @@ public class Prova {
 		String a4 = "Java";
 		
 		//"What is the output?
-		System.out.println(a1 == a2);      //  -> false		
+		System.out.println(a1 == a2);      //  -> false. Pq compara referencia		
 		System.out.println(a1 == a3);      //  -> true
-		System.out.println(a1.equals(a2)); //  -> true
-		System.out.println(a1 == a4);      //  -> true
+		System.out.println(a1.equals(a2)); //  -> true. Pq compara conteúdo
+		System.out.println(a1 == a4);      //  -> true. A Classe Wraper String identifica o mesmo nome
 	}
 	
 }
--- src/critical/techworks/teste2/Q03Driver.java
+++ src/critical/techworks/teste2/Q03Driver.java
@@ -2,11 +2,11 @@ package critical.techworks.teste2;
 
 import java.util.Objects;
 
-public class Driver {
+public class Q03Driver {
 	
 	private String name;
 	
-	Driver(String name) {
+	Q03Driver(String name) {
 	  this.name = name;
 	}
 	public String getName() {
@@ -25,9 +25,9 @@ public class Driver {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!(obj instanceof Driver))
+		if (!(obj instanceof Q03Driver))
 			return false;
-		Driver other = (Driver) obj;
+		Q03Driver other = (Q03Driver) obj;
 		return Objects.equals(name, other.name);
 	}
 }
